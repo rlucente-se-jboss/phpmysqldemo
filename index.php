3cdf3863eafe52b30b47c4ddad0677d247bb2ca6
rlucente-se-jboss <rlucente@redhat.com>
2020-02-24 14:19:16 -0500

    add error handling

--- index.php
+++ index.php
@@ -62,7 +62,9 @@ if ($conn) {
 
   mysqli_close($conn);
 } else {
-  echo "Database is not available";
+  echo "Database is not available" . PHP_EOL;
+  echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+  echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
 }
 
 echo "<p><i>";
